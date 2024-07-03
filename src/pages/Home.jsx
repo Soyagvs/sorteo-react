82a4eac2303e0773158a979fe9873ff1ccf409c3
Soyagvs <agustinmartinezyt@gmail.com>
2024-07-02 22:38:00 -0300

    add mediaquerys

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { TableList } from "../components/table/TableList"
 
 export const Home = () => {
     return (
-        <div className="flex flex-col justify-center items-center mt-20 gap-10">
+        <div className="flex flex-col justify-center items-center mt-40 gap-10">
             <h1 className="text-white font-bold text-pretty max-sm:text-5xl max-lg:text-8xl lg:text-9xl text-center max-sm:w-[100%]">Bienvenidos a SORTEOS</h1>
             <p className="text-gray-500 text-2xl text-center w-[700px] max-md:w-[600px] max-sm:w-[300px] mx-52">Debes ingresar los nombres de las personas de las cuales quieres que participen, buena suerte!</p>
             <div className="flex flex-col justify-center items-center gap-5">
