61275568ae3e0e6a72bf64defcb0e1e37bf9ac34
Soyagvs <agustinmartinezyt@gmail.com>
2024-07-02 22:55:59 -0300

    array responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Footer from "./components/footer/Footer"
 import { Home } from "./pages/Home"
 
 const App = () => {
   return (
     <>
       <Home />
+      <Footer />
     </>
   )
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='flex  flex-col justify-center items-center bg-black h-20 mt-20'>
+          <p>Echa por <a href="https://www.instagram.com/soyagvs/" target='_blank' className='text-blue-800 font-semibold' >Agustin Martinez</a></p>
+      <p>2024 &copy; | Todos los derechos reservados.</p>
+    </footer>
+  )
+}
+
+export default Footer
